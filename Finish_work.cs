9a601939b9965de2eb23d756e6d0cd312bbd8064
MarinaIvanova <imv027@yandex.ru>
2023-09-07 15:55:18 +0400

    Made a second array

--- Finish_work.cs
+++ Finish_work.cs
@@ -1,3 +1,15 @@
-﻿string[] array1 = new string[7] {"Samara", "1234", "eng", "0", "Moscow", "-7", "rus"};
-string[] array2 = new string[array1.Lenght];
+﻿string[] arr1 = new string[7] {"Samara", "1234", "eng", "0", "Moscow", "-7", "rus"};
+string[] arr2 = new string[arr1.Lenght];
 
+void MakeSecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for(int i = 0; i < arr1.Lenght; i++)
+    {
+        if(arr1.Lenght[i] <= 3)
+        {
+            arr2[count] = arr1[i];
+            count ++;
+        }
+    }
+}
