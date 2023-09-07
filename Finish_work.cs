be1fdcd757b7f645e5fb168d5f324e5d61afaa2a
MarinaIvanova <imv027@yandex.ru>
2023-09-07 16:13:41 +0400

    Finishwork is done

--- Finish_work.cs
+++ Finish_work.cs
@@ -1,12 +1,12 @@
 ﻿string[] arr1 = new string[7] {"Samara", "1234", "eng", "0", "Moscow", "-7", "rus"};
-string[] arr2 = new string[arr1.Lenght];
+string[] arr2 = new string[arr1.Length];
 
 void MakeSecondArray(string[] arr1, string[] arr2)
 {
     int count = 0;
-    for(int i = 0; i < arr1.Lenght; i++)
+    for(int i = 0; i < arr1.Length; i++)
     {
-        if(arr1.Lenght[i] <= 3)
+        if(arr1[i].Length <= 3)
         {
             arr2[count] = arr1[i];
             count ++;
@@ -15,7 +15,10 @@ void MakeSecondArray(string[] arr1, string[] arr2)
 }
 void ShowArray(string[] arr)
 {
-    for(int i = 0; i < arr.Lenght; i++)
+    for(int i = 0; i < arr.Length; i++)
         Console.Write(arr[i] + " ");
     Console.WriteLine();
 }
+MakeSecondArray(arr1, arr2);
+ShowArray(arr2);
+
